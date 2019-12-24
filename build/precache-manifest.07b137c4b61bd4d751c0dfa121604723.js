d9160d9bcfe09f22acf3aaeb8263b34372374c9b
Marta <martaprietoluque@gmail.com>
2019-12-24 03:24:19 +0100

    Add build docs https

---
+++ build/precache-manifest.07b137c4b61bd4d751c0dfa121604723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed97f40e8b5f2b39936e4363528a674",
+    "url": "/api/v1/reports/index.html"
+  },
+  {
+    "revision": "c95759e7fffc59d46bcf",
+    "url": "/api/v1/reports/static/css/main.4bb706ca.chunk.css"
+  },
+  {
+    "revision": "8fd462e9b27bb27979a8",
+    "url": "/api/v1/reports/static/js/2.41a4196f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/api/v1/reports/static/js/2.41a4196f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c95759e7fffc59d46bcf",
+    "url": "/api/v1/reports/static/js/main.8ecebf02.chunk.js"
+  },
+  {
+    "revision": "7c1cd4ca6bc10d539ad4",
+    "url": "/api/v1/reports/static/js/runtime-main.620dfb18.js"
+  }
+]);
