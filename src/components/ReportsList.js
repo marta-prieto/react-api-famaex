c1a9ac370d8b7e05919f5861cff809f43ae00754
Marta <martaprietoluque@gmail.com>
2019-12-24 02:56:41 +0100

    Add readme and favicon

--- src/components/ReportsList.js
+++ src/components/ReportsList.js
@@ -7,29 +7,29 @@ const ReportsList = props => {
 
   return (
     <main className="main__app">
-    <section className="section__app">
-      <ul className="main__list-reports">
-        {reports
-          .filter(filterReports => filterReports.id.includes(queryData))
-          .map(item => {
-            return (
+      <section className="section__app">
+        <ul className="main__list-reports">
+          {reports
+            .filter(filterReports => filterReports.id.includes(queryData))
+            .map(item => {
+              return (
 
-              <li className="list__reports" key={item.id} style={{ gridColumnStart: 'span 1', gridColumnEnd: 'span 2' }}>
-                <ReportsCard
-                  id={item.id}
-                  name={item.name}
-                  text={item.Text}
-                />
-              </li>
+                <li className="list__reports" key={item.id} style={{ gridColumnStart: 'span 1', gridColumnEnd: 'span 2' }}>
+                  <ReportsCard
+                    id={item.id}
+                    name={item.name}
+                    text={item.Text}
+                  />
+                </li>
 
-            );
-          })}
-      </ul>
-    </section>
-        <footer className="footer__app"> 
-          <span className="footer__title">footer |</span>
-          
-          <nav className="footer__nav">
+              );
+            })}
+        </ul>
+      </section>
+      <footer className="footer__app">
+        <span className="footer__title">footer |</span>
+
+        <nav className="footer__nav">
           <ul className="footer__menu">
             <li className="footer__list">
               <a className="footer__link" href="/">con |</a>
@@ -41,20 +41,16 @@ const ReportsList = props => {
               <a className="footer__link" href="/">links</a>
             </li>
           </ul>
-        </nav>   
-        
-    </footer>
+        </nav>
+
+      </footer>
     </main>
-    
+
   );
 };
 
 ReportsList.propTypes = {
   reports: PropTypes.arrayOf(PropTypes.object).isRequired,
   queryData: PropTypes.string.isRequired
-  /* id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  group: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired */
 };
 export default ReportsList;
